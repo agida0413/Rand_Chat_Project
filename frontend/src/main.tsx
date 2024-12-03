7ac971bb8c5d91e4f4212b9232ba8d26d9860759
km6293 <alstn7616@naver.com>
2024-12-03 14:36:52 +0900

    feat: react query 전역 error notify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { notify } from '@/utils/toast'
 
 import { GlobalStyle } from '@/styles/global'
 import theme from '@/styles/theme'
 import Router from './routes'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: error => {
+      notify('error', error.message)
+    }
+  }),
+  mutationCache: new MutationCache({
+    onError: error => {
+      notify('error', error.message)
+    }
+  })
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <ToastContainer />
         <GlobalStyle />
         <Router />
       </ThemeProvider>
