cc6dd8d18588f5d70737cce6be5c425aa3663e04
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-12-08 20:06:44 +0900

    매칭서버 통합테스트,이미지배포

--- backend/rand-chat-matching/src/test/java/it/MatchingItTest.java
+++ backend/rand-chat-matching/src/test/java/it/MatchingItTest.java
@@ -4,25 +4,31 @@ package it;
 import com.rand.member.model.Members;
 import com.rand.member.model.cons.MembersSex;
 import com.rand.member.repository.MemberRepository;
+
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
 
 import java.time.LocalDate;
 
 @SpringBootTest
 public class MatchingItTest {
 
+
+
     @Autowired
     private MemberRepository memberRepository;
     @Test
     public void test(){
+
         Members members = new Members();
 
-        members.setUsername("test123123");
-                   members.setPassword("rla!104294242");
-                   members.setEmail("agida0413123@naver.com");
+        members.setUsername("agida0413");
+      members.setPassword("rla!1042922");
+                   members.setEmail("agida0413@naver.com");
                    members.setNickName("아아아아아아메리");
                    members.setSex(MembersSex.MAN);
                    members.setBirth(LocalDate.now());
@@ -32,7 +38,7 @@ public class MatchingItTest {
 
         Members findMembers = memberRepository.findByEmail(members);
 
-        Assertions.assertThat(findMembers).isNotNull();
+        Assertions.assertThat(findMembers).isNull();
 
     }
 }
